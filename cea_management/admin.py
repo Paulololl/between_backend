7f5f42bb174afd74719666e94b0b7d07624443ed
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-04-23 03:41:32 +0800

    added filter department by school and filter program by department in admin panel.

--- cea_management/admin.py
+++ cea_management/admin.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
-
 from .models import (SchoolPartnershipList, Department, Program, School)
 
 
-model_to_register = [SchoolPartnershipList, Department, Program, School]
+model_to_register = [SchoolPartnershipList, School]
 
 for model in model_to_register:
     admin.site.register(model)
 
 
+@admin.register(Department)
+class CustomDepartment(admin.ModelAdmin):
+    model = Department
+
+    list_display = ('department_name', 'school')
+
+    list_filter = ('school',)
+
+    fieldsets = (
+        (None, {'fields': ('school', 'department_name')}),
+    )
+
+
+@admin.register(Program)
+class CustomProgram(admin.ModelAdmin):
+    model = Program
+
+    list_display = ('program_name', 'department', 'school_name')
+
+    list_filter = ('department',)
+
+    fieldsets = (
+        (None, {'fields': ('department', 'program_name',)}),
+    )
+
+    def school_name(self, obj):
+        return obj.department.school.school_name
