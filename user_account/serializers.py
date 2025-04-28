e6a8bcb361f5c25fee99acf28857c95e92c956b4
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-04-28 18:34:35 +0800

    adde email to company serializer

--- user_account/serializers.py
+++ user_account/serializers.py
@@ -552,10 +552,11 @@ class GetApplicantSerializer(serializers.ModelSerializer):
 
 class GetCompanySerializer(serializers.ModelSerializer):
     verified_at = serializers.DateTimeField(source='user.verified_at')
+    email = serializers.EmailField(source='user.email')
 
     class Meta:
         model = Company
-        fields = ['user', 'company_name', 'company_address', 'company_information', 'business_nature',
+        fields = ['user', 'email', 'company_name', 'company_address', 'company_information', 'business_nature',
                   'company_website_url', 'linkedin_url', 'facebook_url', 'instagram_url', 'x_url',
                   'other_url', 'background_image', 'profile_picture', 'verified_at']
 
