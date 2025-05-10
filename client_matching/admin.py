96ca26bda8aba073780ab5fdf806dd5207d0f0ba
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-05-10 17:14:12 +0800

    added custom admin panel view for internshippostings

--- client_matching/admin.py
+++ client_matching/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import (HardSkillsTagList, SoftSkillsTagList, InternshipPosting, InternshipRecommendation,
                      Report, MinQualification, Benefit, Advertisement, KeyTask, PersonInCharge)
 
 
-model_to_register = [InternshipPosting, InternshipRecommendation,
+model_to_register = [InternshipRecommendation,
                      Report, MinQualification, Benefit, Advertisement, KeyTask, PersonInCharge]
 
 for model in model_to_register:
@@ -44,6 +45,82 @@ class CustomDepartment(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('lightcast_identifier', 'name')}),
     )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+
+@admin.register(InternshipPosting)
+class CustomInternshipPosting(admin.ModelAdmin):
+    model = InternshipPosting
+
+    list_display = ('internship_posting_id', 'company')
+
+    readonly_fields = (
+        'internship_posting_id',
+        'company',
+        'display_hard_skills',
+        'display_soft_skills',
+        'display_key_tasks',
+        'display_min_qualifications',
+        'display_benefits',
+    )
+
+    fieldsets = (
+        (None, {
+            'fields': ('internship_posting_id', 'company', 'person_in_charge', 'internship_position',
+                       'address', 'other_requirements', 'is_paid_internship', 'is_only_for_practicum',
+                       'internship_date_start', 'application_deadline', 'date_created', 'date_modified'),
+        }),
+        ('Benefits', {
+            'fields': ('display_benefits',),
+        }),
+        ('Key Tasks', {
+            'fields': ('display_key_tasks',),
+        }),
+        ('Min Qualifications', {
+            'fields': ('display_min_qualifications',),
+        }),
+        ('Skills', {
+            'fields': ('display_hard_skills', 'display_soft_skills'),
+        }),
+    )
+
+    def display_hard_skills(self, obj):
+        skills = "<br>".join([skill.name for skill in obj.required_hard_skills.all()])
+        return mark_safe(skills)
+
+    display_hard_skills.short_description = "Hard Skills"
+
+    def display_soft_skills(self, obj):
+        skills = "<br>".join([skill.name for skill in obj.required_soft_skills.all()])
+        return mark_safe(skills)
+
+    display_soft_skills.short_description = "Soft Skills"
+
+    def display_key_tasks(self, obj):
+        tasks = "<br>".join([task.key_tasks for task in obj.keytask_set.all()])
+        return mark_safe(tasks)
+
+    display_key_tasks.short_description = "Key Tasks"
+
+    def display_min_qualifications(self, obj):
+        qualifications = "<br>".join([qual.min_qualifications for qual in obj.minqualification_set.all()])
+        return mark_safe(qualifications)
+
+    display_min_qualifications.short_description = "Min Qualifications"
+
+    def display_benefits(self, obj):
+        benefits = "<br>".join([b.benefits for b in obj.benefit_set.all()])
+        return mark_safe(benefits)
+
+    display_benefits.short_description = "Benefits"
 
     def has_add_permission(self, request):
         return False
