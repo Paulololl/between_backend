5813b3914e8b2e1137c902e6fd639ba3ff9d1475
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-05-27 17:09:08 +0800

    minor changes 8D

--- client_application/views.py
+++ client_application/views.py
@@ -73,54 +73,22 @@ class ApplicationDetailView(ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
+        application_id = self.request.query_params.get('application_id')
 
         if user.user_role == 'applicant':
-            queryset = Application.objects.filter(applicant__user=user)
-        elif user.user_role == 'company':
-            queryset = Application.objects.filter(internship_posting__company__user=user)
-        else:
-            return Application.objects.none()
-
-        application_status = self.request.query_params.get('application_status')
-        allowed_status = ['Confirmed', 'Pending', 'Rejected', 'Dropped']
-        if application_status:
-            if application_status not in allowed_status:
-                raise serializers.ValidationError({'error': 'Invalid application status'})
-            queryset = queryset.filter(status__iexact=application_status)
-
-        view_status = self.request.query_params.get('view_status')
-        allowed_view_status = ['Read', 'Unread']
-        if view_status:
-            if view_status not in allowed_view_status:
-                raise serializers.ValidationError({'error': 'Invalid view status'})
-            if view_status == 'Read':
-                if user.user_role == 'applicant':
-                    queryset = queryset.filter(is_viewed_applicant=True)
-                elif user.user_role == 'company':
-                    queryset = queryset.filter(is_viewed_company=True)
-            elif view_status == 'Unread':
-                if user.user_role == 'applicant':
-                    queryset = queryset.filter(is_viewed_applicant=False)
-                elif user.user_role == 'company':
-                    queryset = queryset.filter(is_viewed_company=False)
-
-        date_order = self.request.query_params.get('date_order')
-        allowed_date_order = ['Newest', 'Oldest']
-        if date_order:
-            if date_order not in allowed_date_order:
-                raise serializers.ValidationError({'error': 'Invalid date order'})
-            if date_order == 'Newest':
-                queryset = queryset.order_by('-application_date')
-            elif date_order == 'Oldest':
-                queryset = queryset.order_by('application_date')
-
-        internship_position = self.request.query_params.get('internship_position')
-        if user.user_role == 'company' and internship_position:
-            queryset = queryset.filter(
-                internship_posting__internship_position__iexact=internship_position
+            return Application.objects.filter(
+                applicant__user=user,
+                application_id=application_id
             )
 
-        return queryset
+        elif user.user_role == 'company':
+            return Application.objects.filter(
+                internship_posting__company__user=user,
+                application_id=application_id
+            )
+
+        else:
+            return Application.objects.none()
 
 
 class NotificationView(ListAPIView):
@@ -169,8 +137,8 @@ class UpdateApplicationView(APIView):
 
         new_status = request.data.get('status')
 
-        if new_status not in ['Pending', 'Confirmed', 'Rejected']:
-            return Response({'error': 'Invalid status. You can only set status to Pending, Confirmed, or Rejected.'},
+        if new_status not in ['Confirmed', 'Rejected']:
+            return Response({'error': 'Invalid status. You can only set status to Confirmed or Rejected.'},
                             status=status.HTTP_400_BAD_REQUEST)
 
         serializer = self.serializer_class(application, data={'status': new_status}, partial=True)
