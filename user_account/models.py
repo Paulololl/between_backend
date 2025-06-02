9eb4c50fab5c34b960dff93d474b9a4f7c60eabc
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-06-02 17:25:34 +0800

    made resume defaults

--- user_account/models.py
+++ user_account/models.py
@@ -105,7 +105,7 @@ class Applicant(models.Model):
     academic_program = models.CharField(max_length=100, null=True, blank=True)
     quick_introduction = models.CharField(max_length=500)
 
-    resume = models.FileField(storage=S3Boto3Storage, upload_to=applicant_resume)
+    resume = models.FileField(storage=S3Boto3Storage, upload_to=applicant_resume, default="")
     enrollment_record = models.FileField(storage=S3Boto3Storage,upload_to=applicant_enrollment_record,
                                          null=True, blank=True)
 
