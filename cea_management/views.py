5032483b823a422b19ca041d8a1bfce2ad0e9e20
Kacie Quiogue <kacie.quiogue@benilde.edu.ph>
2025-06-08 16:58:18 +0800

    error handling for company partnerships and students & file upload for add ojt coordinator

--- cea_management/views.py
+++ cea_management/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 from user_account.models import CareerEmplacementAdmin, OJTCoordinator, Applicant, Company
 from user_account.serializers import GetOJTCoordinatorSerializer, OJTCoordinatorRegisterSerializer, GetApplicantSerializer, EditOJTCoordinatorSerializer
-from .models import SchoolPartnershipList
+from .models import SchoolPartnershipList, Program
 from user_account.permissions import IsCEA
 from .serializers import CompanyListSerializer, CreatePartnershipSerializer, SchoolPartnershipSerializer
 
@@ -15,8 +15,6 @@ cea_management_tag = extend_schema(tags=["cea_management"])
 
 
 class CEAMixin:
-    permission_classes = [IsAuthenticated, IsCEA]
-
     # get the cea instance of user & return error if not found
     def get_cea_or_403(self, user):
         try:
@@ -25,10 +23,12 @@ class CEAMixin:
             raise PermissionDenied("User is not a Career Emplacement Admin. Access denied.")
 
 
+# region OJT Coordinators Management
+
+# RETRIEVE List
 @cea_management_tag
-# OJT Coordinators -- KC
-# region
 class OJTCoordinatorListView(CEAMixin, generics.ListAPIView):
+    permission_classes = [IsAuthenticated, IsCEA]
     serializer_class = GetOJTCoordinatorSerializer
 
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
@@ -47,38 +47,53 @@ class OJTCoordinatorListView(CEAMixin, generics.ListAPIView):
         , 'last_name'
         , 'user__email'
         , 'program__program_name'
+        , 'department__department_name'
     ]
 
     ordering = ['user__status', 'program__program_name']
 
     def get_queryset(self):
         cea = self.get_cea_or_403(self.request.user)
-        queryset = OJTCoordinator.objects.filter(program__department__school=cea.school, user__status__in=['Active', 'Inactive', 'Suspended'])
+        queryset = OJTCoordinator.objects.filter(department__school=cea.school, user__status__in=['Active', 'Inactive', 'Suspended'])
 
         user = self.request.query_params.get('user')
-
         if user:
             queryset = queryset.filter(user=user)
 
         return queryset
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        if not queryset.exists():
+            return Response(
+                {'message': 'No OJT Coordinators are currently available.'}
+            )
 
+        return super().list(request, *args, **kwargs)
+
+# CREATE COORDINATOR
 @cea_management_tag
 class CreateOJTCoordinatorView(CEAMixin, generics.CreateAPIView):
+    permission_classes = [IsAuthenticated, IsCEA]
     serializer_class = OJTCoordinatorRegisterSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        cea = self.get_cea_or_403(self.request.user)
+        context['school'] = cea.school
+        return context
 
     @transaction.atomic
     def perform_create(self, serializer):
         cea = self.get_cea_or_403(self.request.user)
+
         program = serializer.validated_data.get('program', None)
+        if program and program.department.school != cea.school:
+            raise PermissionDenied({'program': ["You can only assign coordinators to programs belonging to your school."]})
 
-        if program:
-            if program.department.school != cea.school:
-                raise PermissionDenied("You can only assign coordinators to programs belonging to your school.")
-
-            existing_coordinators = OJTCoordinator.objects.filter(program=program, user__status__in=['Active', 'Inactive', 'Suspended']).first()
-            if existing_coordinators:
-                raise ValidationError({"program": ["This program already has an assigned OJT coordinator."]})
+        department = serializer.validated_data.get('department', None)
+        if not department or department.school != cea.school:
+            raise ValidationError({'department': ["The chosen department must belong to your school."]})
 
         try:
             serializer.save()
@@ -90,53 +105,25 @@ class CreateOJTCoordinatorView(CEAMixin, generics.CreateAPIView):
 
 @cea_management_tag
 class UpdateOJTCoordinatorView(CEAMixin, generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated, IsCEA]
     queryset = OJTCoordinator.objects.all()
     serializer_class = EditOJTCoordinatorSerializer
 
     def get_object(self):
-        user = self.request.query_params.get('user')  # Get the user from the query parameters
+        user = self.request.query_params.get('user')
         if not user:
             raise ValidationError({"error": "Query parameter 'user' is required."})
 
-        # Get the OJT coordinator using the user ID
         try:
-            instance = self.get_queryset().get(user__user_id=user)
+            return self.get_queryset().get(user__user_id=user)
         except OJTCoordinator.DoesNotExist:
             raise ValidationError({"error": f"No OJT Coordinator found for user: {user}"})
 
-        return instance
-
     def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        coordinator = self.get_object()
+        serializer = self.get_serializer(coordinator, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
 
-        # Retrieve the program from the request data, if provided
-        program = serializer.validated_data.get('program', None)
-
-        # Check attempted status change
-        new_status = request.data.get('status', '').capitalize()
-        current_status = instance.user.status
-
-        # Prevent transition from 'Deleted' to any other status unless enforced by an admin
-        if current_status == 'Deleted' and new_status != 'Deleted':
-            raise PermissionDenied(
-                "You cannot reactivate an OJT Coordinator who has been deleted. Please contact an administrator."
-            )
-
-        # If attempting to mark as 'Deleted', ensure permission to do so
-        if new_status == 'Deleted':
-            raise PermissionDenied(
-                "You cannot directly mark an OJT Coordinator as 'Deleted'. Contact an administrator.")
-
-        if program:
-            # Ensure no other Active/Inactive coordinator is assigned to the program
-            existing_coordinators = OJTCoordinator.objects.filter(program=program, user__status__in=['Active', 'Inactive', 'Suspended']).exclude(pk=instance.pk).first()
-
-            if existing_coordinators:
-                raise ValidationError({"program": ["This program already has an assigned OJT coordinator."]})
-
-        # Proceed with the update
         serializer.save()
 
         return Response(serializer.data)
@@ -144,6 +131,7 @@ class UpdateOJTCoordinatorView(CEAMixin, generics.UpdateAPIView):
 
 @cea_management_tag
 class RemoveOJTCoordinatorView(CEAMixin, generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated, IsCEA]
 
     queryset = OJTCoordinator.objects.all()
     serializer_class = GetOJTCoordinatorSerializer
@@ -165,14 +153,18 @@ class RemoveOJTCoordinatorView(CEAMixin, generics.UpdateAPIView):
         coordinator.user.status = 'Deleted'
         coordinator.user.save()
 
-        return Response({'message': 'The OJT Coordinator has been removed.'})
+        return Response(
+            {'message': 'The OJT Coordinator has been removed.'}
+        )
+
 # endregion
 
 
+# region Student list
+
 @cea_management_tag
-# Student list --KC
-# region
 class ApplicantListView(CEAMixin, generics.ListAPIView):
+    permission_classes = [IsAuthenticated, IsCEA]
     serializer_class = GetApplicantSerializer
 
     filter_backends = [filters.SearchFilter]
@@ -186,15 +178,28 @@ class ApplicantListView(CEAMixin, generics.ListAPIView):
 
     def get_queryset(self):
         cea = self.get_cea_or_403(self.request.user)
-        return Applicant.objects.filter(program__department__school=cea.school, user__status__in=['Active'])
+        return Applicant.objects.filter(school=cea.school, user__status__in=['Active'])
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        if not queryset.exists():
+            return Response(
+                {'message': 'No students currently registered in your school.'}
+            )
+
+        return super().list(request, *args, **kwargs)
 
 # endregion
 
-@cea_management_tag
 # region Company Partnerships -- PAUL
+
+@cea_management_tag
 class SchoolPartnershipListView(CEAMixin, generics.ListAPIView):
+    permission_classes = [IsAuthenticated, IsCEA]
     serializer_class = SchoolPartnershipSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = [ 'company__company_name']
 
     def get_queryset(self):
         cea = self.get_cea_or_403(self.request.user)
@@ -205,21 +210,21 @@ class SchoolPartnershipListView(CEAMixin, generics.ListAPIView):
         return queryset
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
+        queryset = self.filter_queryset(self.get_queryset())
         if not queryset.exists():
             return Response(
                 {'message': 'There are no company partnerships.'},
                 status=status.HTTP_200_OK,
             )
 
-        serilizer = SchoolPartnershipSerializer(queryset, many=True)
-        return Response(serilizer.data, status=status.HTTP_200_OK)
+        serializer = SchoolPartnershipSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @cea_management_tag
 class CreateSchoolPartnershipView(CEAMixin, generics.CreateAPIView):
+    permission_classes = [IsAuthenticated, IsCEA]
     serializer_class = CreatePartnershipSerializer
-    permission_classes = [IsAuthenticated]
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
@@ -234,6 +239,37 @@ class CreateSchoolPartnershipView(CEAMixin, generics.CreateAPIView):
 
         read_serializer = SchoolPartnershipSerializer(partnerships, many=True)
         return Response(read_serializer.data, status=status.HTTP_201_CREATED)
+
+
+@cea_management_tag
+class CompanyListView(CEAMixin, generics.ListAPIView):
+    permission_classes = [IsAuthenticated, IsCEA]
+
+    serializer_class = CompanyListSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['company_name']
+
+    def get_queryset(self):
+        cea = self.get_cea_or_403(self.request.user)
+
+        partnered_company_ids = SchoolPartnershipList.objects.filter(
+            school=cea.school
+        ).values_list('company__user__user_id', flat=True)
+
+        return Company.objects.filter( user__status__in=['Active']
+                                ).exclude(user__user_id__in=partnered_company_ids
+                                ).select_related('user')
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        if not queryset.exists():
+            return Response(
+                {'message': 'No Companies found.'},
+                status=status.HTTP_200_OK,
+            )
+
+        serializer = CompanyListSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @cea_management_tag
@@ -274,32 +310,6 @@ class BulkDeleteSchoolPartnershipView(CEAMixin, generics.GenericAPIView):
         )
 
 
-@cea_management_tag
-class CompanyListView(CEAMixin, generics.ListAPIView):
-    serializer_class = CompanyListSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['company_name']
 
-    def get_queryset(self):
-        cea = self.get_cea_or_403(self.request.user)
-
-        partnered_company_ids = SchoolPartnershipList.objects.filter(
-            school=cea.school
-        ).values_list('company__user__user_id', flat=True)
-
-        return Company.objects.filter( user__status__in=['Active']
-                                ).exclude(user__user_id__in=partnered_company_ids
-                                ).select_related('user')
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        if not queryset.exists():
-            return Response(
-                {'message': 'No Companies found.'},
-                status=status.HTTP_200_OK,
-            )
-
-        serializer = CompanyListSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
 # endregion
