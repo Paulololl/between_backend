696f53382a69d274bfc1b6c884e60130e50c2cd2
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-06-10 16:30:44 +0800

    added fixess

--- client_matching/serializers.py
+++ client_matching/serializers.py
@@ -226,7 +226,7 @@ class CreateInternshipPostingSerializer(serializers.ModelSerializer):
                 'internship_date_start': 'Internship start date must be a future date.'
             })
 
-        if attrs.get('application_deadline') <= attrs.get('internship_date_start'):
+        if attrs.get('application_deadline') > attrs.get('internship_date_start'):
             raise serializers.ValidationError({
                 'application_deadline': 'application deadline must be less than internship date start.'
             })
@@ -411,7 +411,7 @@ class EditInternshipPostingSerializer(serializers.ModelSerializer):
                 'internship_date_start': 'Internship start date must be a future date.'
             })
 
-        if attrs.get('application_deadline') <= attrs.get('internship_date_start'):
+        if attrs.get('application_deadline') > attrs.get('internship_date_start'):
             raise serializers.ValidationError({
                 'application_deadline': 'application deadline must be less than internship date start.'
             })
