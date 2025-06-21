bb3bf86f292f3619607edcc9a6d7185fd82a8107
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-06-22 01:31:57 +0800

    f

--- between_ims/settings.py
+++ between_ims/settings.py
@@ -152,9 +152,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'between_ims' / 'staticfiles',
-]
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
