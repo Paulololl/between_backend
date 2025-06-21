1e3d3fa00ee7e191334f1e055a38a4347893fca4
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-06-21 22:35:17 +0800

    allowed hosts

--- between_ims/settings.py
+++ between_ims/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["172.30.0.1", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["172.30.0.1", "127.0.0.1", "localhost", "between-backend.up.railway.app"]
 
 # Application definition
 
