366eef2c5673d061965dc5171020e546bf24f019
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-06-23 23:32:21 +0800

    fixes

--- client_matching/apps.py
+++ client_matching/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
 
-from client_matching.utils import get_sentence_model
-
 
 class ClientMatchingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'client_matching'
-
-    def ready(self):
-        get_sentence_model()
-
