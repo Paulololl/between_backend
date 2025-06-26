8b4ed62549bd1685c51f1ff37a48cbd7fd66c3a4
Paulololl <134380871+Paulololl@users.noreply.github.com>
2025-06-26 14:22:32 +0800

    made program and departmend editable in admin panel

--- user_account/admin.py
+++ user_account/admin.py
@@ -59,7 +59,7 @@ class ApplicantAdmin(admin.ModelAdmin):
 
     readonly_fields = (
         'user', 'first_name', 'last_name', 'middle_initial',
-        'school', 'department', 'program',
+        'school',  # School and Department was here
         'academic_program', 'address', 'preferred_modality',
         'quick_introduction', 'display_hard_skills', 'display_soft_skills',
         'resume', 'last_matched', 'last_recommendation_filter_state', 'latitude', 'longitude'
