662024b7c38d7aba1b2f9501a809bfff7149e73b
Luhpett <paultagalakad@gmail.com>
2025-09-21 09:45:13 +0800

    debug=True

--- between_ims/settings.py
+++ between_ims/settings.py
@@ -30,7 +30,7 @@ load_dotenv(dotenv_path=os.path.join('wwwroot', '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["172.30.0.1", "127.0.0.1", "localhost", "between-backend.up.railway.app"]
 
